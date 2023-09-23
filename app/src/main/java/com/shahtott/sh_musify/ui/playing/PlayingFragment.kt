34cb94f116c456eb002a850971116d814eeed66f
Elshahatt <MahmoudElshahatt>
2023-09-23 07:34:17 -0700

    show media session notification

--- app/src/main/java/com/shahtott/sh_musify/ui/playing/PlayingFragment.kt
+++ app/src/main/java/com/shahtott/sh_musify/ui/playing/PlayingFragment.kt
@@ -1,13 +1,19 @@
 package com.shahtott.sh_musify.ui.playing
 
+import android.content.ComponentName
 import android.graphics.Bitmap
 import android.media.session.MediaSession
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
 import androidx.fragment.app.viewModels
+import androidx.media3.common.MediaItem
+import androidx.media3.common.Player
+import androidx.media3.session.MediaController
+import androidx.media3.session.SessionToken
 import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
+import com.google.common.util.concurrent.MoreExecutors
 import com.shahtott.sh_musify.R
 import com.shahtott.sh_musify.common.core.BaseFragment
 import com.shahtott.sh_musify.common.extentions.onBackPress
@@ -15,6 +21,7 @@ import com.shahtott.sh_musify.common.handler.MainAudioPlayer
 import com.shahtott.sh_musify.common.handler.MusicHandler
 import com.shahtott.sh_musify.common.handler.startAutoTextHorizontalScrolling
 import com.shahtott.sh_musify.databinding.FragmentPlayingBinding
+import com.shahtott.sh_musify.service.PlaybackService
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -23,6 +30,7 @@ class PlayingFragment : BaseFragment<FragmentPlayingBinding>(
     FragmentPlayingBinding::inflate
 ) {
     private val viewModel: PlayingViewModel by viewModels()
+    private lateinit var controller: MediaController
 
     @Inject
     lateinit var myPlayer: MainAudioPlayer
@@ -38,6 +46,24 @@ class PlayingFragment : BaseFragment<FragmentPlayingBinding>(
         observations()
 
     }
+
+    override fun onPause() {
+        super.onPause()
+        val sessionToken = SessionToken(
+            requireContext(),
+            ComponentName(requireContext(), PlaybackService::class.java)
+        )
+        val controllerFuture = MediaController.Builder(requireContext(), sessionToken).buildAsync()
+
+        controllerFuture.addListener(
+            {
+                // init player
+                controller = controllerFuture.get()
+            },
+            MoreExecutors.directExecutor()
+        )
+    }
+
 
     private fun observations() {
         binding.apply {
